c4fec03bc533d94a5329d42b705e80bcb11e6f68
Akash <akashyeole02@gmail.com>
2023-04-28 17:43:50 +0530

    fixed some bugs on usermodels

--- models/UserModel.js
+++ models/UserModel.js
@@ -4,13 +4,8 @@ const userSchema = new mongoose.Schema(
     {
         name: String,
         email: String,
-<<<<<<< HEAD
-        mobile: Number,
-        password: String
-=======
         password: String,
         mobile: Number
->>>>>>> 2d85143dbd22d0b123be1aa31d8c06e1e4bf4736
     }
 );
 
